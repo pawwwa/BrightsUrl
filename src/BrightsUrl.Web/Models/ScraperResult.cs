457c3dad383ed29504ece94b1ba2c1ec43bb52a2
Yan Mahdych <50081168+pawwwa@users.noreply.github.com>
2023-01-07 18:21:07 +0200

    Create ScraperResult.cs

---
+++ src/BrightsUrl.Web/Models/ScraperResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace BrightsUrl.Web.Models
+{
+    public class ScraperResult
+    {
+        public string? Title { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
