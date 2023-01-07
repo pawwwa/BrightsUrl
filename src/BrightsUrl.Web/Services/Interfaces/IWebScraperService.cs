37c803135667b8e8ab242833d74671b969d772d0
Yan Mahdych <50081168+pawwwa@users.noreply.github.com>
2023-01-07 18:20:54 +0200

    Create WebScrapper service

---
+++ src/BrightsUrl.Web/Services/Interfaces/IWebScraperService.cs
@@ -0,0 +1,9 @@
+﻿using BrightsUrl.Web.Models;
+
+namespace BrightsUrl.Web.Services.Interfaces
+{
+    public interface IWebScraperService
+    {
+        Task<ScraperResult> ScrapeTitleAndStatusCode(string url);
+    }
+}
---
+++ src/BrightsUrl.Web/Services/WebScraperService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using BrightsUrl.Web.Models;
+using BrightsUrl.Web.Services.Interfaces;
+
+namespace BrightsUrl.Web.Services
+{
+    public class WebScraperService : IWebScraperService
+    {
+        public async Task<ScraperResult> ScrapeTitleAndStatusCode(string url)
+        {
+            using var client = new HttpClient();
+
+            var response = await client.GetAsync(url);
+
+            var content = await response.Content?.ReadAsStringAsync();
+
+            var title = Regex.Match(content, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>",
+                    RegexOptions.IgnoreCase).Groups["Title"].Value;
+
+            return new ScraperResult
+            {
+                Title = title,
+                StatusCode = response.StatusCode
+            };
+        }
+
+    }
+}
