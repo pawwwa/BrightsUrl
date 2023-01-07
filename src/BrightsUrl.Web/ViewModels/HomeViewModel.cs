f3efe91201ff5d411cc5ae8c9fd1a5909297c0a7
Yan Mahdych <50081168+pawwwa@users.noreply.github.com>
2023-01-07 18:21:47 +0200

    Create HomeViewModel.cs

---
+++ src/BrightsUrl.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BrightsUrl.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string? UrlsToProcess { get; set; }
+        public List<UrlDataViewModel> ProcessedUrls { get; set; } = new();
+    }
+}
