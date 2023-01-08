0c8b279bd45d73082deab93cbb2aded2befb7c29
Yan Mahdych <50081168+pawwwa@users.noreply.github.com>
2023-01-08 17:05:56 +0200

    Add null forgiving statement

--- src/BrightsUrl.Web/ViewModels/UrlDataViewModel.cs
+++ src/BrightsUrl.Web/ViewModels/UrlDataViewModel.cs
@@ -4,7 +4,7 @@ namespace BrightsUrl.Web.ViewModels
 {
     public class UrlDataViewModel
     {
-        public string Url { get; set; }
+        public string Url { get; set; } = null!;
         public string? Title { get; set; }
         public HttpStatusCode StatusCode { get; set; }
     }
