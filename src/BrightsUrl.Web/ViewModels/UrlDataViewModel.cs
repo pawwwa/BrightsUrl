85c165def2a29ff7a77bbb834af422e50552734a
Yan Mahdych <50081168+pawwwa@users.noreply.github.com>
2023-01-08 18:28:49 +0200

    Create models from data

--- src/BrightsUrl.Web/ViewModels/UrlDataViewModel.cs
+++ src/BrightsUrl.Web/ViewModels/UrlDataViewModel.cs
@@ -8,5 +8,11 @@ namespace BrightsUrl.Web.ViewModels
         public string? Title { get; set; }
         public HttpStatusCode StatusCode { get; set; }
         public bool Succeeded { get; set; }
+
+        public static UrlDataViewModel FromSuccess(string url, string? title, HttpStatusCode statusCode)
+            => new UrlDataViewModel { Url = url, Title = title, StatusCode= statusCode, Succeeded = true };
+
+        public static UrlDataViewModel FromError(string url) 
+            => new UrlDataViewModel { Url = url, Succeeded = false };
     }
 }
